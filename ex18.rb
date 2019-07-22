b350cd77b367b321a9642ffea140636c83d572de
Kaleeswaran13 <skaleess96@gmail.com>
2019-07-23 01:15:56 +0530

    second commit

--- ex18.rb
+++ ex18.rb
@@ -4,7 +4,7 @@ def print_two(*args)
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
 
-# ok, that *args is actually pointless, we can just do this
+# ok, that *args is actually pointless, we can just do this.
 def print_two_again(arg1, arg2)
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
